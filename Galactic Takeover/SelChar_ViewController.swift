2d56a0487c4c0f1c2c6b7096afefa0d18a8d9837
Mayra <msanchezk17@gmail.com>
2019-11-26 12:40:58 -0800

    New View Controllers

---
+++ Galactic Takeover/SelChar_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelChar_ViewController.swift
+//  Galactic Takeover
+//
+//  Created by Mayra Sanchez on 11/26/19.
+//  Copyright © 2019 Rachel Bright. All rights reserved.
+//
+
+import UIKit
+
+class SelChar_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
